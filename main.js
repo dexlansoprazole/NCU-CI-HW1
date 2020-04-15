1b0b5dade4f7701695aef6ce33f705f5a14e4626
Dexlansoprazole <prtd.lambo@gmail.com>
2020-04-15 09:45:08 +0800

    refactor: remove dialog

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow, Menu, ipcMain, dialog} = require('electron')
+const {app, BrowserWindow, Menu, ipcMain} = require('electron')
 const fs = require('fs'); 
 const path = require('path'); 
 if (!app.isPackaged)
